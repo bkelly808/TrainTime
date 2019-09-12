e5ed3c2759d0286343730f384e5f91516901ee41
B Kelly <berenicekelly808@gmail.com>
2019-09-11 18:11:07 -0700

    setup firebase and added to app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    // Your web app's Firebase configuration
+ var firebaseConfig = {
+   apiKey: "AIzaSyDKub0Uyq7SZJWEwmnNKeRJ7Ba7okHBsLs",
+   authDomain: "trainhw-22f18.firebaseapp.com",
+   databaseURL: "https://trainhw-22f18.firebaseio.com",
+   projectId: "trainhw-22f18",
+   storageBucket: "trainhw-22f18.appspot.com",
+   messagingSenderId: "385043582868",
+   appId: "1:385043582868:web:9da22f573cd406c0a8c981"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
